7186a814009ee5a91dd8bc813fa5e51202855d96
Daniel Qian <danielqian@Daniels-MacBook-Pro-2.local>
2024-01-11 23:19:25 -0800

    adding to ignore

--- app/page.tsx
+++ app/page.tsx
@@ -30,6 +30,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Title>Surgeon Input</Title>
       <Card>
         <SurgeonTable surgeons={inputSurgeons} />
       </Card>
